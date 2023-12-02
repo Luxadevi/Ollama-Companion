95ba0b48dbad767c94f202f1195eaf71fe0390e9
Your Name <jordibtje@gmail.com>
2023-12-02 12:40:43 +0000

    ad

--- modules/mid_ui.py
+++ modules/mid_ui.py
@@ -6,23 +6,28 @@ def create_mid_ui():
     models_dir = "llama.cpp/models/"
     gguf_files = []
 
-    # Check if the models directory exists
+    # Check if the models directory exists and list .gguf files
     if os.path.exists(models_dir):
-        # Iterate through each model folder
         for model_folder in os.listdir(models_dir):
             hpq_folder = os.path.join(models_dir, model_folder, 'High-Precision-Quantization')
-            # Check for 'High-Precision-Quantization' subfolder and list .gguf files
             if os.path.exists(hpq_folder) and os.path.isdir(hpq_folder):
                 for file in os.listdir(hpq_folder):
                     if file.endswith('.gguf'):
-                        gguf_files.append(f"{model_folder}/High-Precision-Quantization/{file}")
+                        gguf_files.append(os.path.join(model_folder, 'High-Precision-Quantization', file))
     else:
         gguf_files = ["Models directory not found"]
 
-
-    with gr.Blocks() as mid_ui:
-        gr.Markdown("### Mid Level Interface")
-        gr.Dropdown(label="Select a GGUF File", choices=gguf_files)
-        # You can add more UI components here as needed
+    with gr.Blocks() as mid_ui:  # Corrected indentation
+        gr.Markdown("### Mid Precision Quantization")
+        with gr.Row():
+            dropdown = gr.Dropdown(label="Select a GGUF File", choices=gguf_files)
+        with gr.Row():
+            checkboxes = [gr.Checkbox(label=option) for option in shared['checkbox_options']]
+        
+        # Example of a button to handle the selections (implement your own logic)
+        run_button = gr.Button("Run Selected Commands")
+        status_output = gr.Textbox(label="Status")
+        # Placeholder function for button click
+        run_button.click(lambda *args: "Commands queued", inputs=checkboxes, outputs=status_output)
 
     return mid_ui
--- modules/shared.py
+++ modules/shared.py
@@ -1,4 +1,7 @@
 shared = {
+    'checkbox_options': [
+    "Q4_0", "Q4_1", "Q5_0", "Q5_1", "Q2_K", "Q3_K", "Q3_K_S", "Q3_K_M", "Q3_K_L",
+    "Q4_K", "Q4_K_S", "Q4_K_M", "Q5_K", "Q5_K_S", "Q5_K_M", "Q6_K", "Q8_0", "F16", "F32"],
     'api_endpoint': {"url": "http://127.0.0.1:11434"},  # Updated API URL
     'gradio': {},
     'parameters': {
@@ -17,9 +20,6 @@ shared = {
         'num_predict': [256, (128, 512)],
         'top_k': [0, (0, 100)],
         'top_p': [1.0, (0.1, 1.0)],
-        'checkbox_options': [
-        "Q4_0", "Q4_1", "Q5_0", "Q5_1", "Q2_K", "Q3_K", "Q3_K_S", "Q3_K_M", "Q3_K_L",
-        "Q4_K", "Q4_K_S", "Q4_K_M", "Q5_K", "Q5_K_S", "Q5_K_M", "Q6_K", "Q8_0", "F16", "F32"
-    ],
+
     }
 }
