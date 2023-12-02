2e64f20c6e1c69db8f8abb37c5f305b59b6da853
Your Name <jordibtje@gmail.com>
2023-12-02 09:02:46 +0000

    First quant step

--- modules/high_ui.py
+++ modules/high_ui.py
@@ -1,25 +1,51 @@
 import gradio as gr
 import os
+import subprocess
+import threading
+
+def run_command(model_folder, out_type):
+    base_dir = "llama.cpp/models"
+    target_dir = f"{base_dir}/{model_folder}/High-Precision-Quantization"
+    output_file = f"{model_folder}-{out_type}.gguf"
+
+    # Ensure the target directory exists
+    os.makedirs(target_dir, exist_ok=True)
+
+    # Construct the command as a list
+    command = [
+        "python", "llama.cpp/convert.py", os.path.join(base_dir, model_folder),
+        "--outfile", os.path.join(target_dir, output_file),
+        "--outtype", out_type
+    ]
+
+    try:
+        subprocess.run(command, check=True)
+        return "Command completed successfully."
+    except subprocess.CalledProcessError as e:
+        return f"Error in command execution: {e}"
+
 
 def create_high_ui():
     # Path to the directory
     models_dir = "llama.cpp/models/"
 
-    # Check if the directory exists
+    # Check if the directory exists and list folders
     if os.path.exists(models_dir):
-        # List only folders in the directory
         model_folders = [f for f in os.listdir(models_dir) if os.path.isdir(os.path.join(models_dir, f))]
     else:
         model_folders = ["Directory not found"]
 
+    def trigger_command(model_folder, out_type):
+        # Run the command and return the status
+        return run_command(model_folder, out_type)
+
     with gr.Blocks() as high_ui:
         gr.Markdown("### High Level Interface")
-
-        # Dropdown menu populated with folders from the directory
-        gr.Dropdown(label="Select a Model Folder", choices=model_folders)
-
-        # Radio buttons for selection
-        gr.Radio(label="Select Mode", choices=["Q8_0", "fp16", "fp32"], value="Q8_0")
-        # Add more components for High UI here
+        with gr.Row():
+            model_folder_input = gr.Dropdown(label="Select a Model Folder", choices=model_folders)
+            out_type_input = gr.Radio(label="Select Output Type", choices=["Q8_0", "f16", "f32"], value="Q8_0")
+            run_button = gr.Button("Run Command")
+        status_output = gr.Textbox(label="Status")
+        run_button.click(trigger_command, inputs=[model_folder_input, out_type_input], outputs=[status_output])
 
     return high_ui
