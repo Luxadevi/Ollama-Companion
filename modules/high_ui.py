a625fbea76aee198c773dfc937719afc89cf5259
Your Name <jordibtje@gmail.com>
2023-12-02 06:49:37 +0000

    Building UI

--- modules/high_ui.py
+++ modules/high_ui.py
@@ -1,7 +1,25 @@
 import gradio as gr
+import os
 
 def create_high_ui():
-    with gr.Blocks() as block:
+    # Path to the directory
+    models_dir = "llama.cpp/models/"
+
+    # Check if the directory exists
+    if os.path.exists(models_dir):
+        # List only folders in the directory
+        model_folders = [f for f in os.listdir(models_dir) if os.path.isdir(os.path.join(models_dir, f))]
+    else:
+        model_folders = ["Directory not found"]
+
+    with gr.Blocks() as high_ui:
         gr.Markdown("### High Level Interface")
+
+        # Dropdown menu populated with folders from the directory
+        gr.Dropdown(label="Select a Model Folder", choices=model_folders)
+
+        # Radio buttons for selection
+        gr.Radio(label="Select Mode", choices=["Q8_0", "fp16", "fp32"], value="Q8_0")
         # Add more components for High UI here
-    return block
+
+    return high_ui
--- modules/quant_ui.py
+++ modules/quant_ui.py
@@ -1,19 +1,15 @@
 import gradio as gr
-from modules.high_ui import create_high_ui
+from modules.high_ui import create_high_ui 
 from modules.mid_ui import create_mid_ui
 from modules.low_ui import create_low_ui
 
 def create_quant_ui():
-    high_ui = create_high_ui()
-    mid_ui = create_mid_ui()
-    low_ui = create_low_ui()
-
     with gr.Blocks() as quant_ui:
         with gr.Tabs():
             with gr.Tab("High"):
-                high_ui  # use high_ui directly
+                create_high_ui()  # Call the function to create high_ui
             with gr.Tab("Mid"):
-                mid_ui  # use mid_ui directly
+                create_mid_ui()  # Call the function to create mid_ui
             with gr.Tab("Low"):
-                low_ui  # use low_ui directly
+                create_low_ui()  # Call the function to create low_ui
     return quant_ui
