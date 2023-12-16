6d8157ece104fe58993308cc4cff0063b88d95ff
Luxadevi <116653852+Luxadevi@users.noreply.github.com>
2023-12-17 00:20:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,7 +55,6 @@ install_docker() {
     return 0
 }
 
-<<<<<<< HEAD
 # Function to configure Docker
 configure_docker() {
     if [ "$1" != "macos" ]; then
@@ -71,8 +70,6 @@ configure_docker() {
 }
 
 # Function to install essential packages
-=======
->>>>>>> 6f1ea8e81f5cc1ac6bf4b7bdd2ac629fa3728d8b
 install_packages() {
     local packages=("gcc" "make" "aria2" "git")
     local install_needed=false
@@ -91,7 +88,6 @@ install_packages() {
 
     case "$1" in
         debian)
-<<<<<<< HEAD
             sudo apt-get update && sudo apt-get install -y "${packages[@]}"
             ;;
         redhat)
@@ -101,24 +97,8 @@ install_packages() {
             sudo pacman -Syu && sudo pacman -S --noconfirm "${packages[@]}"
             ;;
         macos)
-            brew install "${packages[@]}"
-=======
-            sudo apt-get update
-            sudo apt-get install -y python3 pip gcc make aria2 build-essential pciutils git # Add 'git' here
-            ;;
-        redhat)
-            sudo yum update
-            sudo yum install -y python3 python3-pip gcc make aria2 pciutils git # Add 'git' here
-            ;;
-        arch)
-            sudo pacman -Syu
-            sudo pacman -S --noconfirm python3 python-pip gcc make aria2 base-devel pciutils git # Add 'git' here
-            ;;
-        macos)
-            # Assumes Homebrew is installed
             brew update
-            brew install python3 pip gcc make aria2 cmake pciutils git # Add 'git' here
->>>>>>> 6f1ea8e81f5cc1ac6bf4b7bdd2ac629fa3728d8b
+            brew install "${packages[@]}"
             ;;
         *)
             echo "Unsupported OS for package installation."
@@ -128,12 +108,12 @@ install_packages() {
     echo "Packages installed successfully."
     return 0
 }
+
 # Function to change ownership and permissions of all files and directories
 change_file_ownership() {
     local script_dir="$(dirname "$(realpath "$0")")"
     echo "Changing ownership of all files and directories in $script_dir..."
 
-<<<<<<< HEAD
     # Change ownership to the current user for all files and directories
     find "$script_dir" -exec chown $USER {} \;
 
@@ -144,16 +124,8 @@ change_file_ownership() {
     echo "Ownership and permissions changed successfully."
 }
 
-
-# Function to determine the operating system
 # Function to determine the operating system
 detect_os() {
-    # Check for Jupyter Notebook environment
-    if [ -n "$JUPYTER_RUNTIME_DIR" ]; then
-        echo "jupyter"
-        return 0
-    fi
-
     local os_name=$(uname -s)
     case "$os_name" in
         Darwin)
@@ -179,7 +151,6 @@ detect_os() {
     return 0
 }
 
-
 # Function to clone the llama.cpp repository
 clone_repository() {
     if git clone https://github.com/ggerganov/llama.cpp.git; then
@@ -191,151 +162,63 @@ clone_repository() {
     return 0
 }
 
+# Function to build llama.cpp
 build_llama_cpp() {
-    # Check if the OS is Jupyter
-    if [ "$1" == "jupyter" ]; then
-        # Jupyter Notebook specific build steps
-        if [ -d "/content/Ollama-Companion/llama.cpp" ]; then
-            if make -C /content/Ollama-Companion/llama.cpp; then
-                echo "llama.cpp built successfully in Jupyter Notebook environment."
-            else
-                echo "Failed to build llama.cpp in Jupyter Notebook environment."
-                return 1
-            fi
+    local os="$1"
+    if [ "$os" == "macos" ] && [ -d "./llama.cpp" ]; then
+        cd ./llama.cpp
+
+        # Create a "build" directory if it doesn't exist
+        if mkdir -p build; then
+            echo "Build directory created successfully"
         else
-            echo "/content/Ollama-Companion/llama.cpp directory not found."
-            return 1
+            echo "Failed to create build directory" >&2
         fi
-        return 0
-    fi
 
-    # Existing logic for other environments
-    if [ -d "llama.cpp" ]; then
-        cd llama.cpp
-
-        # Linux: Use make for building
-        if [ "$1" == "debian" ] || [ "$1" == "redhat" ] || [ "$1" == "arch" ]; then
-            if make; then
-                echo "llama.cpp built successfully using make."
-            else
-                echo "Failed to build llama.cpp using make."
-                return 1
-            fi
-        # macOS: Use CMake for building
-        elif [ "$1" == "macos" ]; then
-            mkdir -p build && cd build
-            cmake .. && cmake --build .
-            echo "llama.cpp built successfully using CMake."
-            cd ../..
+        # Run CMake from the "build" directory
+        if cmake ..; then
+            echo "CMake configuration successful"
         else
-            echo "Unsupported OS for building llama.cpp."
-            return 1
+            echo "CMake configuration failed" >&2
         fi
-=======
-# Detect OS
-OS="unknown"
-if [ "$(uname)" == "Darwin" ]; then
-    OS="macos"
-elif [ -f /etc/debian_version ]; then
-    OS="debian"
-elif [ -f /etc/redhat-release ]; then
-    OS="redhat"
-elif [ -f /etc/arch-release ]; then
-    OS="arch"
-elif grep -q Microsoft /proc/version 2>/dev/null; then
-    echo "WOW, you ran a bash script inside Windows. Sadly, Windows is not supported with Ollama but you can still use this web UI to interface with an Ollama endpoint and use the quantize functions."
-    ./window_install.ps1
-    exit 0
-fi
 
-# Ask macOS and Linux users if they want to install or update Ollama
-if [ "$OS" == "macos" ] || [ "$OS" == "debian" ] || [ "$OS" == "redhat" ] || [ "$OS" == "arch" ]; then
-    while true; do
-        read -p "Do you want to install or update Ollama? (install/update/none): " choice
-        case "$choice" in
-            install)
-                # Install Ollama
-                break
-                ;;
-            update)
-                # Update Ollama
-                break
-                ;;
-            none)
-                # Do nothing and exit
-                echo "No action selected. Exiting..."
-                exit 0
-                ;;
-            *)
-                echo "Invalid choice. Please enter 'install', 'update', or 'none'."
-                ;;
-        esac
-    done
-fi
+        # Build the project
+        if cmake --build .; then
+            echo "Build successful"
+        else
+            echo "Build failed" >&2
+        fi
 
-# Clone the llama.cpp repository from GitHub
-git clone https://github.com/ggerganov/llama.cpp.git
+        # Copy the generated files to the main "llama.cpp" folder
+        if cp -r * ..; then
+            echo "Generated files copied successfully"
+        else
+            echo "Failed to copy generated files" >&2
+        fi
 
-# Linux specific installation for Ollama (if selected)
-if [ "$choice" == "install" ] && [ "$OS" == "debian" ] || [ "$OS" == "redhat" ] || [ "$OS" == "arch" ]; then
-    curl -O https://ollama.ai/install.sh
-    chmod +x install.sh
-    ./install.sh
-fi
-
-# Systemd setup for Linux
-if [ "$OS" != "macos" ] && systemctl | grep -q '\-\.mount'; then
-    echo "Adding systemd configuration for Ollama..."
-    mkdir -p /etc/systemd/system/ollama.service.d
-    echo '[Service]' >> /etc/systemd/system/ollama.service.d/environment.conf
-    echo 'Environment="OLLAMA_HOST=0.0.0.0:11434"' >> /etc/systemd/system/ollama.service.d/environment.conf
-
-    echo "Reloading systemd and restarting Ollama..."
-    sudo systemctl daemon-reload
-    sudo systemctl restart ollama
-fi
-
-# Launchd setup for macOS
-if [ "$OS" == "macos" ]; then
-    if command -v launchctl >/dev/null 2>&1; then
-        echo "Adding launchd configuration for Ollama..."
-
-        # Create a plist file for your service
-        cat <<EOF > /Library/LaunchDaemons/com.yourcompany.ollama.plist
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>Label</key>
-    <string>com.yourcompany.ollama</string>
-    <key>ProgramArguments</key>
-    <array>
-        <string>/MacOS/Ollama.app/Contents/MacOS/Ollama</string>
-    </array>
-    <key>EnvironmentVariables</key>
-    <dict>
-        <key>OLLAMA_HOST</key>
-        <string>0.0.0.0:11434</string>
-    </dict>
-    <key>RunAtLoad</key>
-    <true/>
-    <key>KeepAlive</key>
-    <true/>
-</dict>
-</plist>
-EOF
-
-        # Load the launchd configuration
-        launchctl load /Library/LaunchDaemons/com.yourcompany.ollama.plist
-
-        echo "Starting Ollama..."
->>>>>>> 6f1ea8e81f5cc1ac6bf4b7bdd2ac629fa3728d8b
-    else
-        echo "llama.cpp directory not found."
-        return 1
+        # Optional: Clean up the "build" directory (remove it if you don't need it)
+        if rm -r build; then
+            echo "Build directory removed successfully"
+        else
+            echo "Failed to remove build directory" >&2
+        fi
+    elif [ "$os" == "jupyter" ]; then
+        # Assuming /content/Ollama-Companion/llama.cpp is the directory to build in
+        if make -C /content/Ollama-Companion/llama.cpp; then
+            echo "Build for Jupyter successful"
+        else
+            echo "Build for Jupyter failed" >&2
+        fi
+    elif [ "$os" != "macos" ] && [ "$os" != "jupyter" ]; then
+        if make -C /llama.cpp; then
+            echo "Build for other OS successful"
+        else
+            echo "Build for other OS failed" >&2
+        fi
     fi
-    return 0
 }
+
+# Function to install Python requirements
 install_python_requirements() {
     local required_packages=("streamlit" "requests" "flask" "flask-cloudflared" "httpx" "litellm" "huggingface_hub" "asyncio" "Pyyaml" "httpx" "APScheduler" "cryptography" "pycloudflared" "numpy==1.24.4" "sentencepiece==0.1.98" "transformers>=4.34.0" "gguf>=0.1.0" "protobuf>=4.21.0" "torch==2.1.1" "transformers==4.35.2")
 
@@ -348,9 +231,7 @@ install_python_requirements() {
         fi
     done
 
-<<<<<<< HEAD
     echo "All required Python packages installed successfully."
-    return 0
 }
 
 # Function to run the key_generation script
@@ -361,78 +242,12 @@ run_key_generation() {
     # Run the key generation script using the full path to avoid any working directory confusion
     if python3 "./key_generation.py"; then
         echo "Key generation script executed successfully."
-=======
-else
-    # Check for Jupyter
-    if [ "$OS" == "jupyter" ]; then
-        # Set up Ollama.sh for Jupyter
-        echo "Setting up Ollama.sh for Jupyter..."
-        if chmod +x /content/ollama.sh && /content/ollama.sh; then
-            echo "Ollama.sh setup and executed successfully for Jupyter"
-        else
-            echo "Failed to set up and execute Ollama.sh for Jupyter" >&2
-        fi
->>>>>>> 6f1ea8e81f5cc1ac6bf4b7bdd2ac629fa3728d8b
     else
         echo "Key generation script execution failed."
         return 1
     fi
     # Return to the previous directory
     popd > /dev/null
-    return 0
- }
-# Function to prompt for Ollama installation in non-Jupyter environments
-
-# Function to download and run Ollama installer
-prompt_ollama_installation() {
-    while true; do
-        read -p "Do you want to install Ollama? (yes/no): " choice
-        case "$choice" in
-            yes|YES|y|Y)
-                download_and_run_ollama_installer
-                break
-                ;;
-            no|NO|n|N)
-                echo "Skipping Ollama installation."
-                break
-                ;;
-            *)
-                echo "Please answer yes or no."
-                ;;
-        esac
-    done
-}
-
-<<<<<<< HEAD
-# Main script execution
-create_and_activate_virtualenv() {
-    echo "Would you like to create a virtual environment for the Python dependencies? (yes/no)"
-    read -r create_venv_choice
-    if [[ $create_venv_choice =~ ^[Yy]es$ ]]; then
-        echo "Please enter the name for the virtual environment:"
-        read -r venv_name
-        python3 -m venv "$venv_name"
-        # shellcheck disable=SC1090
-        source "$venv_name/bin/activate"
-        if [ $? -ne 0 ]; then
-            echo "Failed to create or activate the virtual environment."
-            return 1
-        fi
-        echo "Virtual environment '$venv_name' created and activated."
-    else
-        echo "Skipping virtual environment setup."
-    fi
-    return 0
-}
-# Function to create a start.sh script
-create_start_script() {
-    if [ -n "$venv_name" ]; then
-        echo "#!/bin/bash" > start.sh
-        echo "source $venv_name/bin/activate" >> start.sh
-        echo "python3 run_app.py" >> start.sh
-        chmod +x start.sh
-        echo "start.sh script created. You can run your application using './start.sh'."
-    fi
 }
 
 # Main script execution
@@ -445,33 +260,36 @@ main() {
         exit 1
     fi
 
-    if [ "$os" != "jupyter" ]; then
-        if ! is_python_installed; then
-            echo "Python 3.10 or later is required but not installed. Exiting."
-            exit 1
-        fi
-
-        create_and_activate_virtualenv
-        if [ $? -ne 0 ]; then
-            echo "Failed to set up virtual environment. Exiting."
-            exit 1
-        fi
-        
-        # Call create_start_script to create the start.sh file
-        create_start_script
+    # Check for Python 3.10 or later
+    if ! is_python_installed; then
+        echo "Python 3.10 or later is required but not installed. Exiting."
+        exit 1
     fi
 
-    # Prompt for Ollama installation if not on Jupyter Notebook
-    if [ "$os" != "jupyter" ]; then
-        prompt_ollama_installation
+    # Check for the presence of Python virtual environment (venv)
+    if ! is_command_installed virtualenv; then
+        echo "Virtualenv is not installed. Please install it using 'pip install virtualenv' and then run this script again."
+        exit 1
     fi
 
+    # Create and activate a Python virtual environment
+    create_and_activate_virtualenv
+
+    # Install Docker and configure it
     install_docker "$os" && configure_docker "$os"
+
+    # Install essential packages
     install_packages "$os"
+
+    # Clone the llama.cpp repository
     clone_repository
+
+    # Build llama.cpp
     build_llama_cpp "$os"
+
+    # Install Python requirements
     install_python_requirements
-    
+
     # Change file ownership and permissions after all operations
     change_file_ownership
 
@@ -482,106 +300,3 @@ main() {
 }
 
 main
-=======
-# Compile llama.cpp if it exists
-# Compile llama.cpp if it exists (macOS-specific)
-if [ "$OS" == "macos" ] && [ -d "./llama.cpp" ]; then
-    cd ./llama.cpp
-
-    # Create a "build" directory if it doesn't exist
-    if mkdir -p build; then
-        echo "Build directory created successfully"
-    else
-        echo "Failed to create build directory" >&2
-    fi
-
-    # Run CMake from the "build" directory
-    if cmake ..; then
-        echo "CMake configuration successful"
-    else
-        echo "CMake configuration failed" >&2
-    fi
-
-    # Build the project
-    if cmake --build .; then
-        echo "Build successful"
-    else
-        echo "Build failed" >&2
-    fi
-
-    # Copy the generated files to the main "llama.cpp" folder
-    if cp -r * ..; then
-        echo "Generated files copied successfully"
-    else
-        echo "Failed to copy generated files" >&2
-    fi
-
-    # Optional: Clean up the "build" directory (remove it if you don't need it)
-    if rm -r build; then
-        echo "Build directory removed successfully"
-    else
-        echo "Failed to remove build directory" >&2
-    fi
-fi
-
-
-    # Build the project
-    if cmake --build .; then
-        echo "Build successful"
-    else
-        echo "Build failed" >&2
-    fi
-
-    # Copy the generated files to the main "llama.cpp" folder
-    if cp -r * ..; then
-        echo "Generated files copied successfully"
-    else
-        echo "Failed to copy generated files" >&2
-    fi
-
-    # Optional: Clean up the "build" directory (remove it if you don't need it)
-    if rm -r build; then
-        echo "Build directory removed successfully"
-    else
-        echo "Failed to remove build directory" >&2
-    fi
-
-fi
-
-# Check if Jupyter is detected and build the files in a specific directory
-if [ "$OS" == "jupyter" ]; then
-    # Assuming /content/Ollama-Companion/llama.cpp is the directory to build in
-    if make -C /content/Ollama-Companion/llama.cpp; then
-        echo "Build for Jupyter successful"
-    else
-        echo "Build for Jupyter failed" >&2
-    fi
-fi
-
-# Check for Linux operating systems and run make
-if [ "$OS" != "macos" ] && [ "$OS" != "jupyter" ]; then
-    if make -C /llama.cpp; then
-        echo "Build for other OS successful"
-    else
-        echo "Build for other OS failed" >&2
-    fi
-fi
-
-# Install Python requirements (if requirements.txt exists)
-if [ -f requirements.txt ]; then
-    if pip install -r requirements.txt; then
-        echo "Python requirements installation successful"
-    else
-        echo "Python requirements installation failed" >&2
-    fi
-fi
-# Run the Python key_generation script
-if python3 key_generation.py; then
-    echo "Key generation script executed successfully"
-else
-    echo "Key generation script execution failed" >&2
-fi
-
-echo "Installation complete."
-echo "Installation complete."
->>>>>>> 6f1ea8e81f5cc1ac6bf4b7bdd2ac629fa3728d8b
